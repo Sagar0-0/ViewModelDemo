acf1d1fc4c4c621cf8722fc0a498e1d6c56305b1
Sagar <sagar.0dev@gmail.com>
2022-08-19 13:18:04 +0530

    live data

--- app/src/main/java/com/example/android/lifecycleaware/MainActivity.kt
+++ app/src/main/java/com/example/android/lifecycleaware/MainActivity.kt
@@ -2,30 +2,35 @@ package com.example.android.lifecycleaware
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
-    private val quoteText: TextView
-        get() = findViewById(R.id.quote)
-    private val authorText: TextView
-        get() = findViewById(R.id.author)
+    private val textView:TextView
+    get() = findViewById(R.id.quote)
+
+    private val editText:EditText
+    get() = findViewById(R.id.edittext)
+
+    private val button:Button
+    get() = findViewById(R.id.button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel= ViewModelProvider(this,MainViewModelFactory(application))[MainViewModel::class.java]
-
-        setQuote(mainViewModel.getQuote())
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel.liveData.observe(this, Observer {
+            //change anything if factlivedata changes
+            textView.text=it
+        })
+        button.setOnClickListener {
+            mainViewModel.updateLiveData(editText.text.toString())
+        }
     }
-    fun setQuote(quote: Quote){
-        quoteText.text=quote.text
-        authorText.text=quote.author
-    }
-
-    fun showPrev(view: View) {setQuote(mainViewModel.showPrev())}
-    fun showNext(view: View) {setQuote(mainViewModel.showNext())}
 
 }
--- app/src/main/java/com/example/android/lifecycleaware/Quote.kt
+++
@@ -1,3 +0,0 @@
-package com.example.android.lifecycleaware
-
-data class Quote(val text:String ,val author:String)
